56fcb75d9fc7d498b88b9ab46df257e5cd72d438
yeabsira_mesfin <yabidesta@gmail.com>
2024-05-29 06:21:01 -0400

    Dynamic values

--- whyreact/src/Header.jsx
+++ whyreact/src/Header.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 
+const reactDescription = ['Fundemental', 'Crucial', 'Core'];
+
+const getRandomInt = (max) => Math.floor(Math.random() * (max+1));
+
 const Header = () => {
   return (
     <div><header>
     <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
     <h1>React Essentials</h1>
     <p>
-      Fundamental React concepts you will need for almost any app you are
-      going to build!
+      {`${reactDescription[getRandomInt(reactDescription.length - 1)]} React concepts you will need for almost any app you are
+      going to build!`} 
     </p>
   </header></div>
   )
