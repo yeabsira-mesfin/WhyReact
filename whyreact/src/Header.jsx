ace7f7ef7c345ab0bbab06235a6995c84d706622
yeabsira_mesfin <yabidesta@gmail.com>
2024-05-29 06:22:16 -0400

    using variables

--- whyreact/src/Header.jsx
+++ whyreact/src/Header.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 const reactDescription = ['Fundemental', 'Crucial', 'Core'];
 
 const getRandomInt = (max) => Math.floor(Math.random() * (max+1));
-
+const description = reactDescription[getRandomInt(reactDescription.length - 1)];
 const Header = () => {
   return (
     <div><header>
     <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
     <h1>React Essentials</h1>
     <p>
-      {`${reactDescription[getRandomInt(reactDescription.length - 1)]} React concepts you will need for almost any app you are
+      {`${description} React concepts you will need for almost any app you are
       going to build!`} 
     </p>
   </header></div>
