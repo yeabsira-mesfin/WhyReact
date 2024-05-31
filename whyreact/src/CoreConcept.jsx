e8c845244ee6cea262fb10fee4500c9b762cd77b
yeabsira_mesfin <yabidesta@gmail.com>
2024-05-31 06:01:42 -0400

    props destructuring

---
+++ whyreact/src/CoreConcept.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+const CoreConcept = ({image, title, description}) => {
+  return <li>
+    <img  src={image}/>
+    <h3>{title}</h3>
+    <p>{description}</p>
+  </li>
+}
+
+export default CoreConcept
